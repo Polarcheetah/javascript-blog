042b229c4ed4a279f92109b210d19c37fcce75a9
Maria Berk <marysia.berk@gmail.com>
2021-12-02 16:41:49 +0100

    Add tag list on the right

--- js/script.js
+++ js/script.js
@@ -47,6 +47,7 @@ const optTitleSelector = '.post-title';
 const optTitleListSelector = '.titles';
 const optArticleTagsSelector = '.post-tags .list';
 const optArticleAuthorSelector = '.post-author';
+const optTagsListSelector = '.tags.list';
 
 function generateTitleLinks(customSelector = ''){
   console.log(`custom selector: ${customSelector}`);
@@ -89,11 +90,13 @@ function generateTitleLinks(customSelector = ''){
 generateTitleLinks();
 
 /***************************************    Tag links    ******************************************/
-
 function generateTags(){
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
-  console.log(articles);
+
   /* START LOOP: for every article: */
   for (let article of articles){
     /* find tags wrapper */
@@ -105,23 +108,24 @@ function generateTags(){
 
     /* get tags from data-tags attribute */
     const articleTags = article.getAttribute('data-tags');
-    console.log(articleTags);
-
+    
     /* split tags into array */
     const articleTagsArray = articleTags.split(' ');
-    console.log(articleTagsArray);
-
+    
     /* START LOOP: for each tag */
-    for (let tag of articleTagsArray) {
-      console.log(tag);
-
+    for (let tag of articleTagsArray){
       /* generate HTML of the link */
       const linkHTML = `<li><a href="#tag-${tag}">${tag}</a></li>`;
-      console.log(linkHTML);
-
+      
       /* add generated code to html variable */
       html = html + linkHTML;
-      console.log(html);
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
+
     /* END LOOP: for each tag */
     }
     /* insert HTML of all the links into the tags wrapper */
@@ -129,9 +133,14 @@ function generateTags(){
     console.log(tagsWrapper);
   /* END LOOP: for every article: */
   }
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
 
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
 }
 generateTags();
+
 
 function tagClickHandler(event){
   /* prevent default action for this event */
@@ -259,3 +268,6 @@ function addClickListenersToAuthors(){
   }
 }
 addClickListenersToAuthors();
+
+
+
